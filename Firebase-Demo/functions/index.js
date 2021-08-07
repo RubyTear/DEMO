5f049151ca6560e31d54adc796104ef0056f3fd8
Kim Xin <rlagmadl@gmail.com>
2021-08-07 23:23:48 +0900

    demo firestore&cloud funtions

--- Firebase-Demo/functions/index.js
+++ Firebase-Demo/functions/index.js
@@ -7,3 +7,27 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// The Firebase Admin SDK to access Firestore.
+const admin = require("firebase-admin")
+admin.initializeApp();
+
+// Take the text parameter passed to this HTTP endpoint and insert it into
+// Firestore under the path /messages/:documentId/original
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+    const original = req.query.text;
+    const writeResult = await admin.firestore().collection('messages').add({original: original})
+
+    await admin.database().collection('messagesDB').add({notoriginal: original})
+
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+})
+
+// Listens for new messages added to /messages/:documentId/original and creates an
+// uppercase version of the message to /messages/:documentId/uppercase
+exports.makeuppercase = functions.firestore.document('messages/{documentId}').onCreate((snap, context) => {
+    const original = snap.data().original
+    functions.logger.log('Uppercasing', context.params.documentId, original)
+    const uppercase = original.toUpperCase();
+    return snap.ref.set({uppercase}, {merge: true});
+})
